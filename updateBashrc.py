8eb30f1c854efc23f5c8061da6ebe3bf538c924d
Arghya Biswas <arghyabiswas05@gmail.com>
2025-04-06 15:18:12 +0530

    Add basic code up update and install color in bashrc
    Signed-off-by: Arghya Biswas <arghyabiswas05@gmail.com>

---
+++ updateBashrc.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+def getFileContent(filePath):
+    """
+    Reads the content of a file and returns it as a list of lines.
+    """
+    with open(filePath, 'r') as file:
+        return file.readlines()
+
+
+def writeFileContent(filePath, content):
+    """
+    Writes a list of lines to a file.
+    """
+    with open(filePath, 'w') as file:
+        file.writelines(content)
+
+
+def main():
+    # Get the path to the user's home directory
+    homeDirectory = os.path.expanduser("~")
+
+    # Define the path to the .bashrc file
+    bashrcPath = os.path.join(homeDirectory, ".bashrc")
+
+    if not os.path.exists(bashrcPath):
+       print(f"{bashrcPath} does not exist.")
+       return
+
+    bashrcContent = getFileContent(bashrcPath)
+    colorCodeBashrcContent = getFileContent("bashrcColorCode")
+    colorCodeBashrcContent[0] = f"\n\n{colorCodeBashrcContent[0]}"
+
+    colorCodeFound = False
+    newBashrcContent = []
+    for line in bashrcContent:
+        if re.search(r"^#{47}\s*BASH\s*COLOR\s*UPDATE\s*SCRIPT\s*#{47}$", line):
+            colorCodeFound = True
+
+        if re.search(r"^#{45}\s*END\s*BASH\s*COLOR\s*UPDATE\s*SCRIPT\s*#{45}$", line):
+            colorCodeFound = False
+            continue
+
+        if  not colorCodeFound:
+            # Remove the line from bashrcContent
+            newBashrcContent.append(line)
+
+    # extend the newBashrcContent with colorCodeBashrcContent
+    newBashrcContent.extend(colorCodeBashrcContent)
+
+    # move the old .bashrc file to .bashrc_old
+    os.rename(bashrcPath, bashrcPath+"_old")
+    writeFileContent(bashrcPath, newBashrcContent)
+
+
+if __name__ == "__main__":
+    main()
